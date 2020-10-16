752dee9241081abd50befc61904665bde177583b
rishav510 <f20180157@pilani.bits-pilani.ac.in>
2020-10-17 00:56:56 +0530

    added comments for better readability

--- correct_id.c
+++ correct_id.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int correct_id(char * s)
 {
-	int count =0;
+	int count =0; // counter variable for calculating the length of input string
 	
-	if(*s >= 48 && *s<=57)
+	if(*s >= 48 && *s<=57) // first char digit check condition
 		return 0;
 	while(*s)
 	{
-		if(!(*s ==95 || (*s>=65 && *s<= 90) || (*s>=97&&*s<=122) || (*s>=48&&*s<=57)))
+		if(!(*s ==95 || (*s>=65 && *s<= 90) || (*s>=97&&*s<=122) || (*s>=48&&*s<=57))) // checking if any of the characters of the name are invalid i.e. special characters. ASCII value of underscore is 95
 			return 0;
 		count++;
 		s++;
@@ -17,7 +17,7 @@ int correct_id(char * s)
 	return 1;
 }
 
-int main()
+int main()	//driver function
 {
 	char string[200];
 	scanf("%s",string);
