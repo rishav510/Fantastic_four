ba270797ac72d6d719d5dfdf48daa802eb87443c
Akash Maurya <52518174+akash-maurya@users.noreply.github.com>
2020-10-21 19:12:09 +0530

    "

---
+++ Driver.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+int main(){
+
+	while(true){
+
+		int input ;
+		scanf("%d",&input);
+
+		switch(input){
+
+			case 0: 
+			      // printf("exit");
+			       break;
+
+			case 1:
+			      // create parse tree 
+					break;
+
+			case 2:
+			/* traverse the parse tree and construct type expression table Also print the type errors while
+			traversing the parse tree and accessing the typeExpressionTable. */
+				   break;
+
+			case 3:
+			// print parse tree in specified format
+			       break;
+
+			case 4:
+			 //Print typeExpressionTable in the specified format.
+
+
+		}
+	}
+	return 0;
+}
