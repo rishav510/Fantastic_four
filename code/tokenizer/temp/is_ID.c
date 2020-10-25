25235e70bd063daecdf1fec18ab51e00e1e854a2
rishav510 <f20180157@pilani.bits-pilani.ac.in>
2020-10-25 13:04:35 +0530

    added new file is_ID.c

---
+++ code/tokenizer/temp/is_ID.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int is_identifier(char* token_string); //returns 1 if token_string is an identifier, 0 otherwise
+
+
+int is_identifier(char* token_string)
+{
+	//write identification logic here
+}
+
